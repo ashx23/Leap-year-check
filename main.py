0a173ac9237103742cb8c20ee6cf49306fd851f0
Tanuj <50631065+ashx23@users.noreply.github.com>
2022-05-26 05:49:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print(f"This {year} is a leap year. ")
+    else :
+      print(f"This {year} is not a leap year")
+  else :
+      print(f"This {year} is a leap year")
+else:
+  print(f"This {year} is not a leap year")
+    
+
+
